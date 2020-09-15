72a6f4ce01b1ff9a65628c026176b4601f23351c
elim.thompson <4-thompsone@users.noreply.gitlab.co-ops.nos.noaa.gov>
2020-09-15 15:23:30 -0400

    clean_data boolean check bug is fixed

--- data_cleaning/clean_data.py
+++ data_cleaning/clean_data.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
     
     ## Clean all stations
     #  1. Default way: include nan VER_WL_VALUE_MSL in counting spikes
-    cleaner.clean_stations (exclude_nan_verified=False)
+    cleaner.clean_stations (exclude_nan_verified=False, station_ids=[8443970])
     #  2. EXCLUDE nan VER_WL_VALUE_MSL in counting spikes
     #cleaner.clean_stations (exclude_nan_verified=True)
 
